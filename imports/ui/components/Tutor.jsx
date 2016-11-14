4c572984a14c578260400f36e14327eb7a858040
Abhishek Shetty <abhishek10.rs@gmail.com>
2016-11-15 01:53:35 +0200

    Revert "merging student dash commits by kalai"

--- imports/ui/components/Tutor.jsx
+++ imports/ui/components/Tutor.jsx
@@ -5,17 +5,14 @@ import BaseComponent from '../components/BaseComponent.jsx';
 
 export default class Tutor extends React.Component {
     render() {
-    if (Meteor.user() && Meteor.user().role === 'student'){
-      var user = Meteor.user();
         return (
-            <div className="student">
-            <p className="tutortitle">Tutor info</p>
+            <div className="Tutor">
                 <div className="tutor-image">
                 <img src="http://userproplugin.com/userpro/wp-content/plugins/userpro/img/default_avatar_male.jpg"
-                     height="80px" width="80px" alt="Tutor Image"/>
-                     <p className="profile-name">{user.profile.name}</p>
-                     <p className="profile-school">{user.profile.major}</p>
-                     <p className="profile-school">{user.profile.school}</p>
+                     height="80px" width="80px" alt="Student Image"/>                     
+                     <p className="profile-name">Mark Alan</p>
+                     <p className="profile-school">Master of Science, CCIS</p>
+                     <p className="profile-school">AaltoCS Guild</p>
                      <img src="https://www.seeklogo.net/wp-content/uploads/2016/09/facebook-icon-preview.png"
                                height="40px" width="40px" alt="Facebook"/>
                      <img src="https://www.seeklogo.net/wp-content/uploads/2015/09/twitter-icon-circle-logo.png"
@@ -23,11 +20,5 @@ export default class Tutor extends React.Component {
                 </div>
             </div>
         );
-    }
-    return (
-        <div>
-        </div>
-    );
-
     }
 }
