36f7ad99dfa310dccb857fe86beed55997882759
Kalaiarasan Saminathan <kalaiarasan.saminathan@aalto.fi>
2016-11-15 16:59:53 +0200

    Student home changes

--- imports/ui/components/Tutor.jsx
+++ imports/ui/components/Tutor.jsx
@@ -5,14 +5,17 @@ import BaseComponent from '../components/BaseComponent.jsx';
 
 export default class Tutor extends React.Component {
     render() {
+    if (Meteor.user() && Meteor.user().role === 'student'){
+      var user = Meteor.user();
         return (
-            <div className="Tutor">
+            <div className="student">
+            <p className="tutortitle">Tutor info</p>
                 <div className="tutor-image">
                 <img src="http://userproplugin.com/userpro/wp-content/plugins/userpro/img/default_avatar_male.jpg"
-                     height="80px" width="80px" alt="Student Image"/>                     
-                     <p className="profile-name">Mark Alan</p>
-                     <p className="profile-school">Master of Science, CCIS</p>
-                     <p className="profile-school">AaltoCS Guild</p>
+                     height="80px" width="80px" alt="Tutor Image"/>
+                     <p className="profile-name">{user.profile.name}</p>
+                     <p className="profile-school">{user.profile.major}</p>
+                     <p className="profile-school">{user.profile.school}</p>
                      <img src="https://www.seeklogo.net/wp-content/uploads/2016/09/facebook-icon-preview.png"
                                height="40px" width="40px" alt="Facebook"/>
                      <img src="https://www.seeklogo.net/wp-content/uploads/2015/09/twitter-icon-circle-logo.png"
@@ -20,5 +23,11 @@ export default class Tutor extends React.Component {
                 </div>
             </div>
         );
+    }
+    return (
+        <div>
+        </div>
+    );
+
     }
 }
--- imports/ui/pages/StudentDashboard.jsx
+++ imports/ui/pages/StudentDashboard.jsx
@@ -3,13 +3,14 @@ import ReactDOM from 'react-dom';
 import {Meteor} from 'meteor/meteor';
 import BaseComponent from '../components/BaseComponent.jsx';
 import Tasks from '../components/Tasks.jsx'
+import Events from '../components/Events.jsx'
 
 export default class StudentDashboard extends React.Component {
       render() {
         return (
             <div className="student-view">
-            
               <Tasks/>
+              <Events/>
             </div>
          );
       }
