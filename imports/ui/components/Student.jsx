0d3f900441bdcc72b753263a5f9f69600aef3727
Abhishek Shetty <Abhishek Shetty>
2016-11-15 23:59:59 +0200

    fixed left pane

--- imports/ui/components/Student.jsx
+++ imports/ui/components/Student.jsx
@@ -13,17 +13,17 @@ export default class Student extends React.Component {
         var user = Meteor.user();
         return (
             <div className="student">
-            <p className="studenttitle">Welcome</p>
+            <p className="studenttitle">Hello!</p>
                 <div className="student-image">
                     <img src="http://userproplugin.com/userpro/wp-content/plugins/userpro/img/default_avatar_male.jpg"
                          height="80px" width="80px" alt="Student Image"/>
                     <p className="profile-name">{user.profile.name}</p>
                     <p className="profile-school">{user.profile.major}</p>
                     <p className="profile-school">{user.profile.school}</p>
-                    <img src="https://www.seeklogo.net/wp-content/uploads/2016/09/facebook-icon-preview.png"
-                              height="40px" width="40px" alt="Facebook"/>
-                    <img src="https://www.seeklogo.net/wp-content/uploads/2015/09/twitter-icon-circle-logo.png"
-                              height="38px" width="38px" alt="Twitter"/>
+                    <a href="#"><img src="https://www.seeklogo.net/wp-content/uploads/2016/09/facebook-icon-preview.png"
+                              height="40px" width="40px" alt="Facebook"/></a>
+                    <a href="#"><img src="https://www.seeklogo.net/wp-content/uploads/2015/09/twitter-icon-circle-logo.png"
+                              height="38px" width="38px" alt="Twitter"/></a>
                 </div>
             </div>
         );
