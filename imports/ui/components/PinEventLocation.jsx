5f2a0de87a219627eab609c1e10a18d565b2c888
Kalaiarasan Saminathan <kalaiarasan.saminathan@aalto.fi>
2016-12-22 19:24:10 +0200

    map changes

--- imports/ui/components/PinEventLocation.jsx
+++ imports/ui/components/PinEventLocation.jsx
@@ -4,28 +4,23 @@ import shouldPureComponentUpdate from 'react-pure-render/function';
 class PinEventLocation extends React.Component {
   constructor() {
     super();
+    this.state = { lng: 60.1867, lat:24.8277 };
   }
-
   componentDidMount() {
       var geocoder = new google.maps.Geocoder();
-      var longitude="";
-      var latitude ="";
       geocoder.geocode({ 'address': this.props.address }, function handleResults(results, status) {
         if (status === google.maps.GeocoderStatus.OK) {
-          console.log(results[0].geometry.location.lat());
-          console.log(results[0].geometry.location.lng());
-          longitude = results[0].geometry.location.lng();
-          latitude = results[0].geometry.location.lat();
-        }
-    });
-      console.log(longitude);
-      console.log(latitude);
+          this.setState({ lng : results[0].geometry.location.lng() });
+          this.setState({ lat : results[0].geometry.location.lat() });
+          console.log(this.state.lat)
+         }
+        }.bind(this));
+      console.log(this.state.lat)
       this.map = new google.maps.Map(this.refs.map, {
-      center: new google.maps.LatLng(this.props.longitude,this.props.latitude),
+      center: new google.maps.LatLng(this.state.lng,this.state.lat),
       zoom: 17
     });
   }
-
   render() {
     const mapStyle = {
       width: 500,
@@ -41,8 +36,6 @@ class PinEventLocation extends React.Component {
   }
 }
 propTypes: {
-  longitude: React.PropTypes.string
-  latitude: React.PropTypes.string
   address:React.PropTypes.string
 }
 module.exports = PinEventLocation;
