2be357bb26112ede509383208b84933107a1b2d1
Phu Pham <anphu.1225@gmail.com>
2016-12-28 02:33:12 +0200

    code cleanup

--- imports/ui/components/post/Task.jsx
+++ imports/ui/components/post/Task.jsx
@@ -5,25 +5,23 @@ import BaseComponent from '../BaseComponent.jsx';
 export default class Task extends BaseComponent {
   constructor(props) {
     super(props);
-    }
+  }
 
   render(){
     const { post } = this.props;
-    console.log(post);
     const todos = post.task.todos;
-    console.log(todos);
-    var Todos = [];
-    for (var i=0; i< todos.length; i++) {
+    let Todos = [];
+    for (let i=0; i< todos.length; i++) {
       Todos.push(<li key={todos[i]._id}>{i+1}. {todos[i].text}.</li>);
     }
 
     return (
       <div className="Task">
-      <ul className="todo-list">
-        {Todos}
+        <ul className="todo-list">
+          {Todos}
         </ul>
         <button className="custom-btn-success">Mark as complete</button>
-</div>
+      </div>
     );
   }
 }
