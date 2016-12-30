b4b4a5e0357395c9c1eb0597f740e206ec42f60d
Abhishek Shetty <abhishek10.rs@gmail.com>
2016-12-30 17:48:15 +0200

    stats fix for profile page

--- imports/ui/containers/ProfilePageContainer.jsx
+++ imports/ui/containers/ProfilePageContainer.jsx
@@ -2,19 +2,21 @@ import { Meteor } from 'meteor/meteor';
 import { createContainer } from 'meteor/react-meteor-data';
 import ProfilePage from '../pages/ProfilePage.jsx';
 import { Groups } from '../../api/groups/groups.js';
+import { Posts } from '../../api/posts/posts.js';
 
 const ProfilePageContainer = createContainer(({params: { id }}) => {
   const allUserDataHandle = Meteor.subscribe('allUserData');
   const groupsHandle = Meteor.subscribe('groups.all');
-  const loading = !(allUserDataHandle.ready() && groupsHandle.ready());
-
+  const postsHandle = Meteor.subscribe('posts.query');
+  const loading = !(allUserDataHandle.ready() && groupsHandle.ready() && postsHandle.ready());
   const user = Meteor.users.findOne(id);
   const userExists = !loading && !!user;
 
   return {
     loading,
     user,
-    group: userExists ? Groups.findOne(user.group) : {}
+    group: userExists ? Groups.findOne(user.group) : {},
+
   };
 }, ProfilePage);
 
