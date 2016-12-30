96939873e459357e704bc84ef46e1cb667da36b0
Abhishek Shetty <abhishek10.rs@gmail.com>
2016-12-30 15:06:48 +0200

    dummy page css fixes

--- imports/ui/pages/Faq.jsx
+++ imports/ui/pages/Faq.jsx
@@ -8,26 +8,27 @@ export default class Faq extends BaseComponent {
             <div id="faq">
                 <br/><br/>
                 <p className="faq-title">Frequently Asked Questions </p>
-                &nbsp;
+                <br />
+                <br />
                 <div className="questions">
-                    <p className="faq-question">How do I sign in to freshmen guide.?</p>
+                    <p className="faq-question">How do I sign in to freshmen guide?</p>
                     <ul>
                     <li className="faq-answer">
-                        Study coordinator will create an account for you freshmen guide application.
+                        Study coordinator will create an account for you on the freshmen guide application.
                     </li>
                     <li className="faq-answer">
-                        He/She shares the credentials through your Registered E-mail id.
+                        He/She shares the credentials to your Registered E-mail id.
                     </li>
                     </ul>
                     <br/>
-                    <p className="faq-question">How do I know about the events organized in Aalto university.?</p>
+                    <p className="faq-question">How do I know about the events organized in Aalto university?</p>
                     <ul>
                     <li className="faq-answer">
                         Tutor of your assigned group or Study Coordinator publishes the event details through freshmen web.
                     </li>
                     </ul>
                     <br/>
-                    <p className="faq-question">Who will be creating the groups.?</p>
+                    <p className="faq-question">Who will be creating the groups?</p>
                     <ul>
                     <li className="faq-answer">
                         Study Coordinator is responsible for creating create groups and assign tutors to the group.
@@ -35,7 +36,7 @@ export default class Faq extends BaseComponent {
                     </ul>
 
                     <br/>
-                    <p className="faq-question">Is it possible that a group to have more than One Tutor.?</p>
+                    <p className="faq-question">Is it possible that a group to have more than One Tutor?</p>
                     <ul>
                     <li className="faq-answer">
                         Yes, There can be be more than one tutor for a group
@@ -43,7 +44,7 @@ export default class Faq extends BaseComponent {
                     </ul>
                     <br/>
 
-                    <p className="faq-question">How do I reset the deafult password Genereated by the Study Coordinator.?</p>
+                    <p className="faq-question">How do I reset the deafult password Genereated by the Study Coordinator?</p>
                     <ul>
                     <li className="faq-answer">
                         Once you are logged in, My profile page allows you to change the default password.
@@ -51,18 +52,18 @@ export default class Faq extends BaseComponent {
                     </ul>
 
                     <br/>
-                    <p className="faq-question">Am I allowed to change my Email id.?</p>
+                    <p className="faq-question">Am I allowed to change my Email id?</p>
                     <ul>
                     <li className="faq-answer">
-                        No, You can't change the Email Id, Programme name and your role.
+                        No, You can't change the Email Id, programme name and your role.
                     </li>
                     </ul>
                     <br/>
 
-                    <p className="faq-question">Can I maintain a personal Todo for the tasks.?</p>
+                    <p className="faq-question">Can I share my personal to-do with everyone?</p>
                     <ul>
                     <li className="faq-answer">
-                        Yes, Students and Tutors can create their private todos.
+                        Yes, students and tutors can create their private todos and click on 'make public' to make it available for everyone to view.
                     </li>
                     </ul>
                     <br/>
